89883bbb42d8df22a7463ac375d3b9a8f818b479
Hodlevska <73657261+Hodlevska@users.noreply.github.com>
2024-01-26 18:45:40 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Біла дошка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2547453370627988,
+        "pitch": 0.07874277383224104,
+        "fov": 1.3360112054562112
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9537563767829926,
+          "pitch": 0.2647798947138149,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.29213826911772856,
+          "pitch": 0.014677540528428068,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Вихід</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.6199348714231654,
+          "pitch": 0.03463927395768529,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Їдальня</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.059694149418366,
+          "pitch": 0.029485377600535045,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Туалет</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.885507810589246,
+          "pitch": 0.030040614011250355,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Кухня</font></font>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "Біля дверей",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9455538280668474,
+        "pitch": 0.1691473114521802,
+        "fov": 1.2931179605513077
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.180383478990967,
+          "pitch": 0.24530002959167874,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5666937027498307,
+          "pitch": 0.031816259879150266,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Вихід</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.027514616715922813,
+          "pitch": -0.006962295616222747,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Їдальня</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.9829080847361205,
+          "pitch": 0.010816395789332134,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Кухня</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.6608804998995659,
+          "pitch": 0.004508605382708808,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Туалет</font></font>",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
